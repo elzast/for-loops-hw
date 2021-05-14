4dc957760aa500e231ac2ba7ca07a2810d2e735f
Eli <ezsto92@gmail.com>
2021-05-14 16:07:18 -0400

    Need to finish hw 3 and 4

---
+++ for-loops-hw/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace for_loops_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1: Create a small Console application that asks a user for a number
+            //The application then counts backward from the users’  number
+            //Challenge: Research the thread.sleep method and have it control the rate at which the numbers are printed to the Console.
+            //Console.WriteLine("Select a number");
+            //int userNumber = int.Parse(Console.ReadLine());
+
+            //for (int i = userNumber; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //    Thread.Sleep(1000);
+            //}
+            //
+            //
+            //Exercise 2: Create a small application that counts by 2’s starting at 0 and goes to 100
+            //for (int i = 0; i <= 100; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //
+            //
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("XOXOXOXO");
+                Console.WriteLine("OXOXOXOX");
+                ;
+            }
+        }
+    }
+}
